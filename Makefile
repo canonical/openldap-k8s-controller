2c07f4721df088bd93e036f2f22bcd452f946ae6
Dave Lawson <dave.lawson@canonical.com>
2020-08-11 16:44:03 -0400

    Initial ultra-simple image build and boilerplate to pass configuration through to the pod

---
+++ Makefile
@@ -0,0 +1,45 @@
+DIST_RELEASE ?= focal
+DOCKER_DEPS = slapd
+
+blacken:
+	@echo "Normalising python layout with black."
+	@tox -e black
+
+
+lint: blacken
+	@echo "Running flake8"
+	@tox -e lint
+
+# We actually use the build directory created by charmcraft,
+# but the .charm file makes a much more convenient sentinel.
+unittest: openldap.charm
+	@tox -e unit
+
+test: lint unittest
+
+clean:
+	@echo "Cleaning files"
+	@git clean -fXd
+
+bind.charm: src/*.py requirements.txt
+	charmcraft build
+
+image-deps:
+	@echo "Checking shellcheck is present."
+	@command -v shellcheck >/dev/null || { echo "Please install shellcheck to continue ('sudo snap install shellcheck')" && false; }
+
+image-lint: image-deps
+	@echo "Running shellcheck."
+	@shellcheck image-scripts/dump-environment.sh
+
+image-build: image-lint
+	@echo "Building the image."
+	@docker build \
+		--no-cache=true \
+		--build-arg BUILD_DATE=$$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+		--build-arg PKGS_TO_INSTALL='$(DOCKER_DEPS)' \
+		--build-arg DIST_RELEASE=$(DIST_RELEASE) \
+		-t openldap:$(DIST_RELEASE)-latest \
+		.
+
+.PHONY: blacken lint unittest test clean image-deps image-lint image-build
--- src/charm.py
+++ src/charm.py
@@ -4,37 +4,127 @@
 
 import logging
 
-from ops.charm import CharmBase
+from ops.charm import (
+    CharmBase,
+    CharmEvents,
+)
 from ops.main import main
-from ops.framework import StoredState
+from ops.framework import (
+    StoredState,
+    EventBase,
+    EventSource,
+)
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    MaintenanceStatus,
+)
 
 logger = logging.getLogger(__name__)
 
 
-class CharmK8SOpenldapCharm(CharmBase):
+REQUIRED_SETTINGS = ['admin_password']
+
+
+class OpenLDAPDBMasterAvailableEvent(EventBase):
+    pass
+
+
+class OpenLDAPCharmEvents(CharmEvents):
+    """Custom charm events."""
+
+    db_master_available = EventSource(OpenLDAPDBMasterAvailableEvent)
+
+
+class OpenLDAPK8sCharm(CharmBase):
     _stored = StoredState()
+
+    on = OpenLDAPCharmEvents()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
 
-    def _on_config_changed(self, _):
-        current = self.model.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+        self.framework.observe(self.on.start, self.configure_pod)
+        self.framework.observe(self.on.config_changed, self.configure_pod)
+        self.framework.observe(self.on.leader_elected, self.configure_pod)
+        self.framework.observe(self.on.upgrade_charm, self.configure_pod)
 
-    def _on_fortune_action(self, event):
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
-        else:
-            event.set_result(
-                "Probability is too important to be left to the experts."
-                " -- Richard Hamming")
+    def _check_for_config_problems(self):
+        """Check for some simple configuration problems and return a
+        string describing them, otherwise return an empty string."""
+        problems = []
+
+        missing = self._missing_charm_settings()
+        if missing:
+            problems.append('required setting(s) empty: {}'.format(', '.join(sorted(missing))))
+
+        return '; '.join(filter(None, problems))
+
+    def _missing_charm_settings(self):
+        """Check configuration setting dependencies and return a list of
+        missing settings; otherwise return an empty list."""
+        config = self.model.config
+        missing = []
+
+        missing.extend([setting for setting in REQUIRED_SETTINGS if setting not in config])
+
+        return sorted(list(set(missing)))
+
+    def _make_pod_spec(self):
+        """Return a pod spec with some core configuration."""
+        config = self.model.config
+        image_details = {
+            'imagePath': config['image_path'],
+        }
+        if config['image_username']:
+            image_details.update(
+                {'username': config['image_username'], 'password': config['image_password']}
+            )
+        pod_config = self._make_pod_config()
+
+        return {
+            'version': 3,  # otherwise resources are ignored
+            'containers': [
+                {
+                    'name': self.app.name,
+                    'imageDetails': image_details,
+                    'ports': [{'containerPort': 389, 'protocol': 'TCP'}],
+                    'envConfig': pod_config,
+                    'kubernetes': {
+                        'readinessProbe': {'tcpSocket': {'port': 389}},
+                    },
+                }
+            ],
+        }
+
+    def _make_pod_config(self):
+        """Return an envConfig with some core configuration."""
+        config = self.model.config
+        pod_config = {}
+
+        if config['admin_password']:
+            pod_config['admin_password'] = config['admin_password']
+
+        return pod_config
+
+    def configure_pod(self, event):
+        """Assemble the pod spec and apply it, if possible."""
+        if not self.unit.is_leader():
+            self.unit.status = ActiveStatus()
+            return
+
+        problems = self._check_for_config_problems()
+        if problems:
+            self.unit.status = BlockedStatus(problems)
+            return
+
+        self.unit.status = MaintenanceStatus('Assembling pod spec')
+        pod_spec = self._make_pod_spec()
+
+        self.unit.status = MaintenanceStatus('Setting pod spec')
+        self.model.pod.set_spec(pod_spec)
+        self.unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
-    main(CharmK8SOpenldapCharm)
+    main(OpenLDAPK8sCharm)
