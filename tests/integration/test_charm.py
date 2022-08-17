ed61a9b2fa56caa0ce58c2e7483e35010cb8d842
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2022-08-17 12:22:10 +0300

    tests(integration): Pass in the openldap image

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -17,7 +17,9 @@ async def test_build_and_deploy(ops_test: OpsTest):
     openldap_charm = await ops_test.build_charm(".")
     await asyncio.gather(
         ops_test.model.deploy("postgresql-k8s", application_name=PSQL, num_units=1),
-        ops_test.model.deploy(openldap_charm, application_name=APP_NAME, num_units=1),
+        ops_test.model.deploy(openldap_charm,
+                              resources={'openldap-image': 'openldapcharmers/openldap:2.4.50'},
+                              application_name=APP_NAME, num_units=1),
     )
     await ops_test.model.wait_for_idle(apps=[APP_NAME, PSQL])
     assert ops_test.model.applications[APP_NAME].status == "waiting"
