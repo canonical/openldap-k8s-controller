5edd1f33342ac3aef6323ad08c86e766e775ead6
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2022-08-18 15:26:53 +0300

    tests(integration): Infer resource from metadata.yaml

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,25 +2,28 @@
 
 import asyncio
 import logging
+from pathlib import Path
 
 import pytest
-from pytest_operator.plugin import OpsTest
+import yaml
 from ops.model import ActiveStatus, WaitingStatus
+from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-APP_NAME = "openldap"
+APP_NAME = "openldap-k8s"
 PSQL = "postgresql-k8s"
 
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     openldap_charm = await ops_test.build_charm(".")
+    openldap_image = yaml.safe_load(Path("metadata.yaml").read_text())["resources"]["openldap-image"]["upstream-source"]
     await asyncio.gather(
         ops_test.model.deploy("postgresql-k8s", application_name=PSQL, num_units=1),
         ops_test.model.deploy(
             openldap_charm,
-            resources={'openldap-image': 'openldapcharmers/openldap:2.4.50'},
+            resources={'openldap-image': openldap_image},
             application_name=APP_NAME,
             num_units=1,
         ),
