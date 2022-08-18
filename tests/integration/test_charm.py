d389c18b8f5173ac136934cc5773b9ca891fae84
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2022-08-18 13:59:25 +0300

    tests(integration): Use <class>.name instead of string

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,6 +5,7 @@ import logging
 
 import pytest
 from pytest_operator.plugin import OpsTest
+from ops.model import ActiveStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
@@ -25,17 +26,17 @@ async def test_build_and_deploy(ops_test: OpsTest):
         ),
     )
     await ops_test.model.wait_for_idle(apps=[APP_NAME, PSQL])
-    assert ops_test.model.applications[APP_NAME].status == "waiting"
-    assert ops_test.model.applications[PSQL].status == "active"
+    assert ops_test.model.applications[APP_NAME].status == WaitingStatus.name
+    assert ops_test.model.applications[PSQL].status == ActiveStatus.name
 
     await ops_test.model.add_relation(APP_NAME + ":db", PSQL + ":db")
     await ops_test.model.wait_for_idle(apps=[APP_NAME, PSQL])
-    assert ops_test.model.applications[APP_NAME].status == "active"
-    assert ops_test.model.applications[PSQL].status == "active"
+    assert ops_test.model.applications[APP_NAME].status == ActiveStatus.name
+    assert ops_test.model.applications[PSQL].status == ActiveStatus.name
 
 
 @pytest.mark.abort_on_fail
 async def test_maintenance_without_postgresql(ops_test: OpsTest):
     await asyncio.gather(ops_test.model.applications[PSQL].remove())
     await ops_test.model.wait_for_idle(apps=[APP_NAME])
-    assert ops_test.model.applications[APP_NAME].status == "waiting"
+    assert ops_test.model.applications[APP_NAME].status == WaitingStatus.name
